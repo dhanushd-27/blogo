a2edbf3e4065fd2efaa27efb2a99b26935e0b4cc
dhanushd-27 <dhanushsai2005@gmail.com>
2025-08-17 12:50:12 +0530

    feat: run the pg on docker container and connect to it using pgxpool

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"blogo/internal/config"
+	"blogo/internal/db"
 	"log"
 	"net/http"
 
@@ -9,16 +10,23 @@ import (
 )
 
 func main() {
-	config, err := config.Load()
+	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
+
+	db, err := db.Connect(cfg)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	log.Println("Connected to database")
+	defer db.Close()
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":" + config.Port))
+	e.Logger.Fatal(e.Start(":" + cfg.Port))
 
-	log.Printf("Server is running on port %s", config.Port)
+	log.Printf("Server is running on port %s", cfg.Port)
 }
