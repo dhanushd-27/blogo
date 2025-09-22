3b20e5d28ea320f199cf42381b666d1b2c7049af
dhanushd-27 <dhanushsai2005@gmail.com>
2025-09-22 21:31:02 +0530

    Mention all blog test funcs

--- internal/handlers/blog_test.go
+++ internal/handlers/blog_test.go
@@ -1,1 +1,13 @@
 package handlers
+
+import "testing"
+
+func TestGetAllBlogs(t *testing.T) {}
+
+func TestGetBlogByID(t *testing.T) {}
+
+func TestCreateBlog(t *testing.T) {}
+
+func TestUpdateBlog(t *testing.T) {}
+
+func TestDeleteBlog(t *testing.T) {}
