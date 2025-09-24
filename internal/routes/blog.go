2ecf7f063c616555f5c3396626ae73aa5d26ccd3
dhanushd-27 <dhanushsai2005@gmail.com>
2025-09-24 14:40:39 +0530

    Update Middleware responses and Blog Routes

--- internal/routes/blog.go
+++ internal/routes/blog.go
@@ -11,13 +11,13 @@ import (
 func BlogRoutes(e *echo.Echo, h blog.BlogHandler, cfg *config.Config) {
 	// Public route for getting all blogs (no auth required)
 	e.GET("/blogs", h.GetAllBlogs)
-	e.GET("/blogs/:id", h.GetBlog)
+	e.GET("/blogs/:id", h.GetBlogByID)
 
 	// Protected routes that require authentication
 	blogGroup := e.Group("/blogs")
 	blogGroup.Use(middleware.JWTCookieMiddleware(cfg.JWTSecret))
 
 	blogGroup.POST("", h.CreateBlog)
-	blogGroup.PUT("/:id", h.UpdateBlog)
+	blogGroup.PATCH("/:id", h.UpdateBlog)
 	blogGroup.DELETE("/:id", h.DeleteBlog)
 }
